74ed1e8b054bf1ced7c78004a9eb9c3f0afd4367
Diogo gomes <diogogomes467@gmail.com>
2025-03-04 17:17:11 -0400

    first commit

---
+++ src/main/java/loja/virtual_java/loja_treinamento/LojaTreinamentoApplication.java
@@ -0,0 +1,15 @@
+package loja.virtual_java.loja_treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTreinamentoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LojaTreinamentoApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/loja/virtual_java/loja_treinamento/LojaTreinamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package loja.virtual_java.loja_treinamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaTreinamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
